8cab34b8a9b426c565b314fe6032b71a58f805a9
Olga Trofimova <k12to1@yandex.ru>
2024-08-17 12:53:30 +0300

    проект по запросу шуток

---
+++ JokeGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JokeGenerator
+//
+//  Created by Olga Trofimova on 17.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
